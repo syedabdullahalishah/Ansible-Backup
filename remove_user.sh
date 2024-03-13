aef3bea3b913030cf041d1f9532a748f4ceac7e1
root <root@ip-172-31-7-93.ap-south-1.compute.internal>
2024-03-13 08:00:35 +0000

    configured remove_user.yml playbook

---
+++ remove_user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Execute Ansible playbook
+echo "~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~"
+echo "COMMENCING DEVOPS USER DELETION"
+echo "~.~.~.~.~.~.~.~.~.~.~.~.~.~.~.~"
+echo ""
+echo ""
+
+#echo "\\\DELETING DEVOPS USER//"
+ansible-playbook -i /ddevops/ansible-playbooks/hosts  /ddevops/ansible-playbooks/devops/remove_user.yml
